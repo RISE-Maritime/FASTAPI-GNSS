49c556935aa14b607f9cb607c3629b5633fbfa4c
TedSjoblom <36690474+TedSjoblom@users.noreply.github.com>
2025-03-31 07:41:54 +0200

    keelson addded

--- README.md
+++ README.md
@@ -26,5 +26,57 @@ Received POST lat: 57.43587153498083, long 12.032563565298915, time: 2025-03-20T
 Received POST request body: b'lat=57.43587153498083&lon=12.032563565298915&sat=0&desc=&alt=72.0&acc=7.0&dir=0.0&prov=gps&spd_kph=0.2520000010728836&spd=0.07&timestamp=1742503647&timeoffset=2025-03-20T21:47:27.834%2B01:00&time=2025-03-20T20:47:27.834Z&starttimestamp=1742499925&date=2025-03-20&batt=54.0&ischarging=false&aid=089db14317f6af11&ser=089db14317f6af11&act=&filename=20250320&profile=Default+Profile&hdop=0.4&vdop=0.8&pdop=1.0&dist=1062&'
 Parsed body: {'lat': ['57.43587153498083'], 'lon': ['12.032563565298915'], 'sat': ['0'], 'alt': ['72.0'], 'acc': ['7.0'], 'dir': ['0.0'], 'prov': ['gps'], 'spd_kph': ['0.2520000010728836'], 'spd': ['0.07'], 'timestamp': ['1742503647'], 'timeoffset': ['2025-03-20T21:47:27.834+01:00'], 'time': ['2025-03-20T20:47:27.834Z'], 'starttimestamp': ['1742499925'], 'date': ['2025-03-20'], 'batt': ['54.0'], 'ischarging': ['false'], 'aid': ['089db14317f6af11'], 'ser': ['089db14317f6af11'], 'filename': ['20250320'], 'profile': ['Default Profile'], 'hdop': ['0.4'], 'vdop': ['0.8'], 'pdop': ['1.0'], 'dist': ['1062']}
 Parsed body LAT: ['57.43587153498083']
+
+
+
+{ 
+    # POS
+    'lat': '57.43596067652106', # Deg 
+    'lon': '12.032632464542985', # Deg
+    'sat': '27', 
+    'alt': '74.0', # Meters 
+    'acc': '4.0', # Meters 
+    
+    # Log / Annotation
+    'desc': '',  # Description text with note icon in app
+
+    # Position Source Satellite 
+    'hdop': '0.4', 
+    'vdop': '0.6', 
+    'pdop': '0.8', 
+    
+    # Device ID
+    'profile': 'Default+Profile', 
+
+    # Time UTC
+    'time': '2025-03-30T11:51:35.000Z', 
+    
+    # Trajectory over ground 
+    'dir': '0.0', # deg
+    'spd': '0.0',  m/s
+    
+    # Battery
+    'batt': '45.0', # Percentage 
+    'ischarging': 'true', 
+    
+
+    'dist': '11432' # meters
+
+
+    
+    # Not parsed to Keelson
+    'timeoffset': '2025-03-30T13:51:35.000%2B02:00', 
+    'timestamp': '1743335495', 
+    'act': '', 
+    'filename': '20250330', 
+    'prov': 'gps', 
+    'starttimestamp': '1743235240', 
+    'date': '2025-03-30', 
+    'aid': '089db14317f6af11', 
+    'ser': '089db14317f6af11', 
+    'spd_kph': '0.0', # w
+    
+}
+
 ```
 
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import logging
 from fastapi import Request
-from urllib.parse import parse_qs
+import zenoh
+import keelson
+from keelson.payloads.LocationFix_pb2 import (
+    PositionFix,
+    PositionSourceSatellites,
+    TraveledDistance,
+)
+from keelson.payloads.Log_pb2 import Log
+from keelson.payloads.Navigation_pb2 import TrajectoryOverGround
+from keelson.payloads.Battery_pb2 import BatteryState
 
 app = FastAPI(
     title="FastAPI",
 )
 
 # Allow external GET requests from 192.168.0.5
-origins = [
-    "http://192.168.0.5",
-    "*"
-]
+origins = ["http://192.168.0.5", "*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,12 +29,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Initialize Zenoh session
+zenoh_session = None
+
+
+# @app.on_event("startup")
+# async def startup_event():
+    
+
+# @app.on_event("shutdown")
+# async def shutdown_event():
+#     global zenoh_session
+#     if zenoh_session:
+#         try:
+#             zenoh_session.close()
+#             print("Zenoh session successfully closed.")
+#         except Exception as e:
+#             print(f"Failed to close Zenoh session: {e}")
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/log")
-async def log(request: Request, lat: float = None, long: float = None, time: str = None):
+async def log(
+    request: Request, lat: float = None, long: float = None, time: str = None
+):
     print(f"Received lat: {lat}, long {long}, time: {time}")
     try:
         body = await request.json()
@@ -39,26 +68,88 @@ async def log(request: Request, lat: float = None, long: float = None, time: str
         "longitude": long,
     }
 
-@app.post("/logg")
-async def log_post(request: Request, lat: float = None, long: float = None, time: str = None):
-    print(f"Received POST lat: {lat}, long {long}, time: {time}")
-    try:
-        #body = await request.body()
-        #print(f"Received POST request body: {body}")
-        #body = await request.json()
-        #print(f"Received body: {body}")
 
+@app.post("/logg")
+async def log_post(
+    request: Request, lat: float = None, long: float = None, time: str = None
+):
+    global zenoh_session
+    print(f"Received POST LOGG at time: {time}")
+    try:
         body = await request.body()
-        print(f"Received POST request body: {body}")
-        parsed_body = parse_qs(body.decode())
-        print(f"Parsed body: {parsed_body}")
-        print(f"Parsed body LAT: {parsed_body.get('lat')}")
+        data = body.decode("utf-8").split("&")
+        parsed_data = {}
+        for item in data:
+            if "=" in item:
+                key, value = item.split("=", 1)
+                parsed_data[key] = value
+
+        print(f"Parsed data: {parsed_data}")
+
+        print(f"LAT: {parsed_data.get('lat')}")
+
+        # PositionFix
+        position_fix = PositionFix(
+            latitude_degrees=float(parsed_data.get("lat",0.0)),
+            longitude_degrees=float(parsed_data.get("lon",0.0)),
+            altitude_meters=float(parsed_data.get("alt",0.0)),
+            accuracy_meters=float(parsed_data.get("acc",0.0)),
+        )
+
+        # Publish data using Zenoh
+        if zenoh_session:
+            key_expr_pos_fix = keelson.construct_pubsub_key(
+                realm="rise",
+                entity_id="web",
+                subject="position_fix",
+                source_id=parsed_data.get("profile"),
+            )
+            pub = zenoh_session.declare_publisher(key_expr_pos_fix)
+            # Publish the target
+            serialized_position_fix = position_fix.SerializeToString()
+            envelope = keelson.enclose(serialized_position_fix)
+            pub.put(envelope)
+            print(f"Published PositionFix to Zenoh with key: {key_expr_pos_fix}")
+        else:
+            print("Zenoh session is not initialized.")
+
+        return parsed_data
+
     except Exception as e:
         print(f"No JSON body received: {e}")
-    return {
-        "lat": lat,
-        "longitude": long,
-    }
-if __name__ == "__main__":
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+def main():
+    global zenoh_session
+    
+    # Setup logger
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s [%(lineno)d]: %(message)s", level=logging.DEBUG
+    )
+    
+    logging.captureWarnings(True)
+    
+
+    try:
+
+        # Construct session
+        logging.info("Opening Zenoh session...")
+        conf = zenoh.Config()
+
+
+        with zenoh.open(conf) as zenoh_session:
+            info = zenoh_session.info
+            logging.info(f"zid: {info.zid()}")
+            logging.info(f"routers: {info.routers_zid()}")
+            logging.info(f"peers: {info.peers_zid()}")
+
+            print("Zenoh session successfully started.")
+
+            uvicorn.run(app, host="0.0.0.0", port=8000)
+          
+    except Exception as e:
+        print(f"Failed to start Zenoh session: {e}")
+
+
+if __name__ == "__main__":
+    main()
